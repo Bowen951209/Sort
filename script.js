99a53f9285fa432613d6b88ecc700896c4216f93
bowen951209 <bowen951209@gmail.com>
2019-06-30 11:47:50 +0800

    Revert "delete trash"
    This reverts commit b71345dc38e71d3dbff669535a0292ab01a0f1ee.

---
+++ script.js
@@ -0,0 +1,37 @@
+var numbers = [2.1, 17, 35, 8];
+
+
+console.log(sort(numbers));
+function sort(arr) {
+    var ranking = [];
+    var len  = arr.length;
+    var under = [];
+    for (let x = 0; x < len; x++) {
+        under.push(0);
+        for (let y = 0; y < len; y++) {
+            if (x + y <= len - 1) {
+                if (arr[x] < arr[x + y]) {
+                    under[x] += 1;
+                    // console.log(x + y);
+                }
+            } else {
+                if (arr[x] < arr[x + y - len]) {
+                    under[x] += 1;
+                    // console.log(x + y - len);
+                }
+            }
+        }
+
+        // console.log(under);
+        // console.log(under.indexOf(0));
+
+
+    }
+
+    for (let i = 0, rankingLength = ranking.length; i < len; i++) {
+        if (under.indexOf(i) > -1) {
+            ranking.unshift(arr[under.indexOf(i)]);
+        }
+    }
+    return ranking;
+}
